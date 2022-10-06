89748fd4c412fc9189adca5cb3494c904f2ef85e
14FU <fauzia53.u@gmail.com>
2022-10-06 12:05:54 +0100

    express

--- exp.js
+++ exp.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const FU = [];
 const app = express();
-app.get('/',(req,res)=>{
+app.get('/FU',(req,res)=>{
     res.send("Hi there im FU")
 });
-let names = [Thom,Shak,Mimi,Luke,Mike];
+let names = ["Thom","Shak","Mimi","Luke","Mike"];
 app.get('/getAll',(req,res)=>{
     res.send(names)
 });
@@ -12,11 +12,17 @@ app.get('/get/:id',(req,res)=>{
     res.send(names[req.params.id])
 
 });
-app.get('/delete/id')
+app.delete('/delete/id',(req,res)=>{
+    res.send(names.splice(req.params.id,2));
+});
+
 app.post("/createFU",(req,res)=>{
      FU.push(req,body);
-     res.status(201).send(ducks);
+     res.status(201).send(FU);
  })
-const server = app.listen(1412, () => console.log(`Server sucessfully started on port ${server.address().port}`));
+
+
+
+const server = app.listen(1412, () => console.log(`Server started on port ${server.address().port}`));
 
 // req= request res= respond/resukt 
