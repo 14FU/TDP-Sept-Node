0aaab3b980f269b9bf533f8186e84389601cf293
14FU <fauzia53.u@gmail.com>
2022-10-11 11:20:14 +0100

    CRUD express

--- exp.js
+++ exp.js
@@ -1,8 +1,3 @@
-//  to intialise it open up new terminal hit dropdown next to + (chnage to bash) 
-// then type npm init -y, to install express type npm install express 
-//^^^ ALL OF THE ABOVE IS DONE IN TERMINAL BASH^^^^
-
-
 // export the js and initialise it 
 const express = require("express");
 const app = express();
@@ -21,10 +16,3 @@ app.use(routes);
 const server = app.listen(4450, () => console.log(`Server started on port ${server.address().port}`));
 // this is the the ocd eot connect to server 
 
-//NOTES 
-// + req= request res= respond
-// + every time you make changes you must Ctrl + C and 
-// + then type *npm start* to send the server live again
-// + pick your own port number (4 digits dob is fine)
-// + With MIDDLEWARE you must always put next () or res.send() or
-// no response will be sent . 
